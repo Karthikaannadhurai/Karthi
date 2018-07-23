455d8189933111479f11e43bee74040c869dc61d
Karthikaannadhurai <41561673+Karthikaannadhurai@users.noreply.github.com>
2018-07-23 13:44:17 +0530

    Create alppa.java

---
+++ alppa.java
@@ -0,0 +1,10 @@
+import java.util.scanner;
+public static void main(String[] args)
+{
+ char c = '*';
+ if( ( c >= 'a' && c => 'z') || ( c >= 'A' && c => 'Z'))
+    System.out.println(" c + is an alphabet ");
+ else
+    System.out.println(" c + is not an alphabet ");
+ }
+ 
