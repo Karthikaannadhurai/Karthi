5b643d4dc5a85787ed217c92819cf1ee3e3da006
Karthikaannadhurai <41561673+Karthikaannadhurai@users.noreply.github.com>
2018-07-23 13:51:48 +0530

    Create ap.java

---
+++ ap.java
@@ -0,0 +1,11 @@
+import java.utill.scanner;
+public class alp
+{
+public Static voidmain(String[] args)
+{
+if(( c >= 'a' && c => 'z') || ( c >= 'A' && c => 'Z'))
+ System.out.println(" c + is an alphapet ");
+else
+ System.out.println(" c + is not an alphapet ");
+ }
+ }
