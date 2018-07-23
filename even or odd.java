55ac1d8d9b9766ca461a75f91b6a9ce4dd4f6df5
Karthikaannadhurai <41561673+Karthikaannadhurai@users.noreply.github.com>
2018-07-23 12:25:16 +0530

    Create even or odd.java

---
+++ even or odd.java
@@ -0,0 +1,20 @@
+import java.util.scanner;
+public static void main(String[] args)
+{
+ int n;
+ scanner s = new scanner(Syatem.in);
+ System.out.print("the given number is integer");
+ n = s.nextINT(); 
+ if(n % 2 == 0)
+ {
+    System.out.println("the given number is even");
+  }
+  elseif
+  {
+   System.out.println("the given number is odd);
+   }
+   else
+   { 
+    System.out.println("the given number is invalid");
+    }
+}
