3f55891ff109078d177fa6e3b8ccbc3234f94cb9
Karthikaannadhurai <41561673+Karthikaannadhurai@users.noreply.github.com>
2018-07-23 14:01:32 +0530

    Create large.java

---
+++ large.java
@@ -0,0 +1,27 @@
+import java.utill.scanner;
+public class alp
+{
+public Static voidmain(String[] args)
+{
+ int a,b,c,largest;
+ scanner s = new scanner(System.in);
+ System.out.println("enter the three number:");
+ a = S.nextInt();
+ b = S.nextInt();
+ c = S.nextInt();
+ if( a>=b && a>=c)
+ {
+ System.out.println("largest number:"+a);
+ }
+ if( b>=a && b>=c)
+ {
+ System.out.println("largest number:"+b);
+ }
+ if( c>=b && c>=a)
+ {
+ System.out.println("largest number:"+c);
+ }
+ }
+ }
+ 
+ 
