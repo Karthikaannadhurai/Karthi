330050eac8ea32b57e2f64b5eb91a6cda850cd0c
Karthikaannadhurai <41561673+Karthikaannadhurai@users.noreply.github.com>
2018-08-01 15:44:31 +0530

    Create sum natural.java

---
+++ sum natural.java
@@ -0,0 +1,15 @@
+public class Demo {
+
+    public static void main(String[] args) {
+
+       int num , count , total = 0;
+
+       while(count <= num)
+       {
+           total = total + count;
+           count++;
+       }
+
+       System.out.println("Sum of first 10 natural numbers is: "+total);
+    }
+}
