02b669cacb751a1d487dddcd56a5d8912a6225c2
Karthikaannadhurai <41561673+Karthikaannadhurai@users.noreply.github.com>
2018-08-01 15:40:40 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,11 @@
+public class hello 
+    {
+        static void Main(string[] args) 
+        {
+            int i,n;
+		        for(i = 0; i <= n; i++)
+		          {
+			          System.out.println("\n Hello World ");
+		          }	
+	      }
+   }
