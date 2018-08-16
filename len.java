afed49c2055ebc97006e0ac2e0ed5b2805ef1d7f
Karthikaannadhurai <41561673+Karthikaannadhurai@users.noreply.github.com>
2018-08-16 16:19:55 +0530

    Create len.java

---
+++ len.java
@@ -0,0 +1,16 @@
+public class AssignmentProgramming
+{
+
+    public static void main(String[] args)
+    { 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a string");
+        String input = sc.nextLine();
+        System.out.println(input);
+        String str = input;
+        String[] myString = str.split(" ");
+        int length = str.length();
+        System.out.println(length);
+    }
+
+}
